796d602aea04785a87a58c6c8a3b33870a93e4ab
higor <higor@DESKTOP-SUEQS7E>
2019-05-21 19:41:45 -0300

    coe

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,8 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("akwpdkaw");
+        
+        System.out.println("higao gostosao");
     }
     
 }
